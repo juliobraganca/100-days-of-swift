d277e6bbcad9e07d77381a0478bf53b35eeb3043
Julio Bragança <jcbbraganca@gmail.com>
2023-03-30 10:58:04 -0300

    Update UserInterfaceState.xcuserstate

---
+++ Day 001 - 15 - Foundation + Consolidation/foundation.playground/Contents.swift
@@ -0,0 +1,41 @@
+//
+<<<<<<< HEAD
+// Day 001 - 015 - Foundation and Consolidation
+=======
+# Day 001 - 015 - Foundation and Consolidation
+>>>>>>> 9efee38c3f671990f296d5dfdf3e02a59f4b2019
+//
+//
+
+import UIKit
+
+<<<<<<< HEAD
+// MARK: Day 13 - Consolidation 1
+
+// MARK: 1. VARIABLES AND CONSTANTS
+
+// Variables (var) can be changed and Constants (let) can't
+// Swift does not let us change a constant
+
+var name = "Tim McGrow"
+name = "Romeo"
+
+let nameConstant = "Tim McGrow"
+
+// Constants - Swifts displays an error if you try to change it
+
+let name2 = "Tim McGrow"
+// ERROR: name2 = "Romeo"
+
+// Variable and constant names must be unique in your code. You'll get an error if you try to use the same variable name twice, like this:
+
+var name3 = "Tim McGraw"
+// ERROR: var name3 = "Romeo"
+
+// MARK: 2. TYPES OF DATA
+=======
+## Day 13 - Consolidation 1
+
+### 1. Variables and Constants
+>>>>>>> 9efee38c3f671990f296d5dfdf3e02a59f4b2019
+
