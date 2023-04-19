3a30e06e42b3abdea59df70941cbca1c229d89e8
Julio Bragança <jcbbraganca@gmail.com>
2023-04-19 13:02:49 -0300

    project 07

---
+++ Project 07/Project 07/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 07
+//
+//  Created by Júlio Bragança on 19/04/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 07/Project 07/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 07
+//
+//  Created by Júlio Bragança on 19/04/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
