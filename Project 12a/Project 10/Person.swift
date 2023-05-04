ff495a060183ff39cf1ad599d617e4b39dafea1d
Julio Bragança <jcbbraganca@gmail.com>
2023-05-04 12:23:20 -0300

    Project 12a - correcting project 10

---
+++ Project 12a/Project 10/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Júlio Bragança on 29/04/23.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    // reading from disk
+    required init?(coder aDecoder: NSCoder) {
+        name =  aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    // writing in disk
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
+}
---
+++ Project 12a/Project 10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Júlio Bragança on 28/04/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+
+}
