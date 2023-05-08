a7d7bf0f18071586dd96bb11d38557040a3a3086
Julio Bragança <jcbbraganca@gmail.com>
2023-05-08 15:51:47 -0300

    Commit

---
+++ Project 06a/Project 13/Project 13/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project 13
+//
+//  Created by Júlio Bragança on 08/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Instafilter"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
